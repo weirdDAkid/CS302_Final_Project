8265097af9fd492a27e1ceb633d18e3b0a6623f7
weirdDAkid <zoya.vincent@yahoo.com>
2020-05-04 22:11:44 -0700

    Adding Cities

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # CS302_Final_Project
+
+Throughout this Final Project:
+    City 'A' = Reno
+    City 'B' = San Francisco
+    City 'C' = Salt Lake City
+    City 'D' = Seattle
+    City 'E' = Las Vegas
+
+Connections between Cities:
+    Reno's (A) Connections:
+        San Francisco (B), weight 218 mi
+        Salt lake city (C), weight 518 miles
+        Seattle (D), weight 704 miles
+        Las Vegas (E), weight 439 miles
+    San Francisco’s (B) connections:
+        Reno (A), weight 218 miles
+        Seattle (D), weight 811 miles
+        Las Vegas (E), weight 564 miles
+    Salt Lake City’s (C) connections:
+        Reno (A), weight 518 miles
+        Seattle (D), weight 829 miles
+        Las Vegas (E), weight 421 miles
+    Seattle’s (D) connections:
+        Reno (A), weight 704 miles
+        San Francisco (B), weight 811 miles
+        Salt Lake City (C), weight 829 miles
+        Las Vegas (E), weight 1,118 miles
+    Las Vegas’s (E) connections:
+        Reno (A), weight 439 miles
+        San Francisco (B), weight 564 miles
+        Seattle (D), weight 1,118 miles
+        Salt Lake City (E), weight 421 miles
--- main.cpp
+++ main.cpp
@@ -5,7 +5,44 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
 
+typedef boost::property<boost::edge_weight_t, int> EdgeWeightProperty;
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, EdgeWeightProperty > DirectedGraph;
+typedef boost::graph_traits<DirectedGraph>::edge_iterator edge_iterator;
+
+/* List of Cities:
+    City 'A' = Reno
+    City 'B' = San Francisco
+    City 'C' = Salt Lake City
+    City 'D' = Seattle
+    City 'E' = Las Vegas
+*/
+
 int main(){
+
+    DirectedGraph cityMap;
+
+    boost::add_edge('A', 'B', 218, cityMap);
+    boost::add_edge('A','C', 518, cityMap);
+    boost::add_edge('A','D', 704, cityMap);
+    boost::add_edge('A','E', 439, cityMap);
+
+    boost::add_edge('B','A', 218, cityMap);
+    boost::add_edge('B','D', 811, cityMap);
+    boost::add_edge('B','E', 564, cityMap);
+
+    boost::add_edge('C','A', 518, cityMap);
+    boost::add_edge('C','D', 829, cityMap);
+    boost::add_edge('C','E', 421, cityMap);
+
+    boost::add_edge('D','A', 704, cityMap);
+    boost::add_edge('D','B', 811, cityMap);
+    boost::add_edge('D','C', 829, cityMap);
+    boost::add_edge('D','E', 1118, cityMap);
+
+    boost::add_edge('E','A', 439, cityMap);
+    boost::add_edge('E','B', 564, cityMap);
+    boost::add_edge('E','C', 1118, cityMap);
+    boost::add_edge('E','D', 421, cityMap);
 
     return 0;
 }
