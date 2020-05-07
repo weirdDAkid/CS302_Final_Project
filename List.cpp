2c22599ecc4d23bb9410f09142a19f319c9b0db5
weirdDAkid <zoya.vincent@yahoo.com>
2020-05-06 23:04:37 -0700

    send help seriously

--- List.cpp
+++ List.cpp
@@ -22,12 +22,12 @@ List<T>::List(const List<T> & other){
         Node<T> *index = head;
         Node<T> *currOther = other.head->getNext();
 
-        while( currOther ){
+        while( currOther != nullptr ){
             std::cout << "heckin copy constructer" << std::endl;
             Node<T> *newNodePtr = new Node<T> ( currOther->getData(), currOther->getNext() );
             index->setNext( newNodePtr );
             currOther = currOther->getNext();
-            if(!currOther){
+            if( currOther == nullptr){
                 tail = index;
             }
             index = index->getNext();
